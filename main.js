76dc5113c7f5d423b10ef98fc4c4d31beb03b5a4
RUAN-ZX <1162179851@qq.com>
2021-03-07 16:18:43 +0800

    首页基本完成 富文本编辑组件基本完成

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ Vue.use(uView);
 
 // 在main.js注册mescroll-body的全局组件 (mescroll-uni用的比较少,且遵循 easycom 规范,会自动按需引入)
 // 注册全局组件
-import MescrollBody from "@/components/mescroll-uni/mescroll-body.vue"
-import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
-Vue.component('mescroll-body', MescrollBody)
-Vue.component('mescroll-uni', MescrollUni)
+// import MescrollBody from "@/components/mescroll-uni/mescroll-body.vue"
+// import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
+// Vue.component('mescroll-body', MescrollBody)
+// Vue.component('mescroll-uni', MescrollUni)
 
 Vue.config.productionTip = false
 
